4c2be9f5ecbf5a2ff58d0059807de6ff17a8172c
zj2626 <ay180629@outlook.com>
2017-09-25 16:53:19 +0800

    .

--- static/code/algorithm/Euclid.java
+++ static/code/algorithm/Euclid.java
@@ -1,7 +1,12 @@
 package algorithm;
 
 /**
- * 求最大公约数      欧几里得算法
+ * 求最大公约数    欧几里得算法
+ * <p>
+ * 最大公约数（Greatest Common Divisor）缩写为GCD
+ * <p>
+ * 定理：两个整数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数 -- gcd(a,b) = gcd(b,a mod b)
+ * <p>
  * Created by zj on 2017/9/25.
  */
 public class Euclid {
---
+++ static/code/structure/Sequence.java
@@ -0,0 +1,8 @@
+package structure;
+
+/**
+ * 顺序表
+ * Created by zj on 2017/9/25.
+ */
+public class Sequence {
+}
